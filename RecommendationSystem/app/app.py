55506cfa25d86e5cdc44c9d4b21751412587b6d3
wesleyjw <jwesleybiologo@gmail.com>
2024-02-18 07:41:33 -0300

    Add app architecture and prototype desing

---
+++ RecommendationSystem/app/app.py
@@ -0,0 +1,14 @@
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
